f42259863b31db6749429e91b563045869f69973
mhuang <mhuang@toshiba.fios-router.home>
2018-01-02 15:18:47 -0500

    first commit

---
+++ src/com/huang/poc/HelloGit.java
@@ -0,0 +1,10 @@
+package com.huang.poc;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Initial Hello GIT");
+	}
+
+}
