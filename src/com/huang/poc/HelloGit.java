4f1c373d67917563fd4713dee85dd3b5db4a6db3
mhuang <mhuang@toshiba.fios-router.home>
2018-01-02 15:46:37 -0500

    invoking module

--- src/com/huang/poc/HelloGit.java
+++ src/com/huang/poc/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Initial Hello GIT");
+helloByName("module1");
 	}
 
 	 public static void helloByName(String name) {
